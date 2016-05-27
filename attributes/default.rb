db0f219f40d8a6fd2749c143cae2a4826f0e1bd5
Paul Bonser <misterpib@gmail.com>
2016-05-27 00:37:27 -0500

    bump build for nil pointer fix

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,7 +4,7 @@
 #
 
 default['heka']['download']['mirror'] = 'http://ownlocal-debs.s3-website-us-east-1.amazonaws.com/heka'
-default['heka']['download']['version'] = '0.9.2-69'
+default['heka']['download']['version'] = '0.9.2-70'
 default['heka']['download']['arch'] = 'amd64'
 default['heka']['download']['extension'] = 'deb'
 default['heka']['download']['remote_src'] = "#{node['heka']['download']['mirror']}/heka_#{node['heka']['download']['version']}_#{node['heka']['download']['arch']}.#{node['heka']['download']['extension']}"
