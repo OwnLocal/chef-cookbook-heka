44a09970038d79f7da1de24371e242de72bf97c5
Paul Bonser <misterpib@gmail.com>
2016-05-30 16:17:44 -0500

    Bump for neverending week fix

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,7 +4,7 @@
 #
 
 default['heka']['download']['mirror'] = 'http://ownlocal-debs.s3-website-us-east-1.amazonaws.com/heka'
-default['heka']['download']['version'] = '0.9.2-71'
+default['heka']['download']['version'] = '0.9.2-73'
 default['heka']['download']['arch'] = 'amd64'
 default['heka']['download']['extension'] = 'deb'
 default['heka']['download']['remote_src'] = "#{node['heka']['download']['mirror']}/heka_#{node['heka']['download']['version']}_#{node['heka']['download']['arch']}.#{node['heka']['download']['extension']}"
